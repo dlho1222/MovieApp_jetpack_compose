16465286dd24b4034f3492150000de19dab800de
LEE HO <dlho1222@naver.com>
2025-02-25 21:56:39 +0900

    Theme adjust

--- app/src/main/java/com/part4/movieapp/ui/components/movie/Category.kt
+++ app/src/main/java/com/part4/movieapp/ui/components/movie/Category.kt
@@ -4,11 +4,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.part4.movieapp.ui.theme.MovieAppTheme
+import com.part4.movieapp.ui.theme.Paddings
 
 @Composable
 fun CategoryRow() {
@@ -43,12 +46,18 @@ fun CategoryRow() {
 fun CategoryTitle(title: String) {
     Text(
         text = "Action",
-        modifier = Modifier.padding(10.dp)
+        modifier = Modifier.padding(
+            vertical = Paddings.large,
+            horizontal = Paddings.extra
+        ),
+        style = MaterialTheme.typography.headlineSmall
     )
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun CategoryPreview() {
-    CategoryRow()
+    MovieAppTheme {
+        CategoryRow()
+    }
 }
